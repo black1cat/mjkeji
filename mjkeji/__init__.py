19a832fed709be28a2dfea441f0cdc5e0db67a05
ms <619149912@qq.com>
2020-10-29 22:12:42 +0800

    first commit

--- mjkeji/__init__.py
+++ mjkeji/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/mjkj?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:971106@localhost:3306/beer?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
